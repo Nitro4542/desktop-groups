34a1fcc15ba6b6ba6be6c3d747f5f3dc19be8ce4
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-09-02 20:04:11 +0200

    Improve group.py

--- desktop-groups/group.py
+++ desktop-groups/group.py
@@ -11,11 +11,9 @@ class Group:
         self.items = []
 
     def add_item(self, name, icon, command):
-        # Adds item to items list
         self.items.append(dict(name = name, icon = icon, command = command))
 
     def remove_item(self, name):
-        # Removes item from items list
         for item in self.items:
             if item['name'] == name:
                 self.items.remove(item)
@@ -41,25 +39,16 @@ class DGFileGroup(Group):
         super().__init__(self.data.get('group', {}).get('name'), self.data.get('group', {}).get('icon'))
 
         # Add items to self.items list
-        self.process_items()
-
-
-    def validate(self):
-        # Try to validate JSON
-        try:
-            jsonschema.validate(self.data, self.schema)
-        except jsonschema.exceptions as e:
-            print(f'Error: {e}')
-            sys.exit(1)
-
-        if self.data.get('group', {}).get('name'):
-            return True
-        else:
-            return False
-
-    def process_items(self):
         group = self.data.get("group", {})
         items = group.get("items", [])
 
         for item in items:
             self.items.append(item)
+
+    def validate(self):
+        try:
+            jsonschema.validate(self.data, self.schema)
+        except jsonschema.exceptions as e:
+            print(f'Error: {e}')
+            return False
+        return True
