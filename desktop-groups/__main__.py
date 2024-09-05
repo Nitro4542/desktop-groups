4aee6373221bb809eeb7bef9be76e821170de303
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-09-05 17:42:37 +0200

    Make it usable

--- desktop-groups/__main__.py
+++ desktop-groups/__main__.py
@@ -1,4 +1,12 @@
 import gui
+import argparse
 
-app = gui.App()
+
+parser = argparse.ArgumentParser(
+                    prog='DesktopGroups',
+                    description='Organizes your desktop')
+parser.add_argument('filename')
+args = parser.parse_args()
+
+app = gui.App(args.filename)
 app.mainloop()
