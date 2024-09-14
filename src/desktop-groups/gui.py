834c20fd9ef5912dfc4b644300ef76b6040e1d23
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2024-09-14 18:35:38 +0200

    Improved code quality

--- src/desktop-groups/gui.py
+++ src/desktop-groups/gui.py
@@ -14,15 +14,40 @@ import customtkinter
 from . import group
 
 
+def _open_icon(icon: str):
+    """Opens an icon and prepares it for PIL
+
+    :return: Icon
+    """
+
+    if icon.endswith('.exe'):
+        try:
+            # Load icon
+            extractor = IconExtractor(icon)
+
+            # Read icon
+            data = extractor.get_icon(num=0)
+
+            return data
+
+        except IconExtractorError:
+            return None
+    else:
+        return icon
+
+
 class GroupItemsScrollableFrame(customtkinter.CTkScrollableFrame):
     """
     Displays all the items in the items list
     """
     def __init__(self, master, items: list, scale: float, base_font: customtkinter.CTkFont, **kwargs):
         """
-        :param master: Root frame
-        :param items: List containing all items
+        :param master: Root frame or parent widget
+        :param items: List of dictionaries, each containing an item with at least 'name' and optionally 'icon' and 'command'.
+        :param scale: Scaling factor for icon size
+        :param base_font: Base font to be used for the radio button text
         """
+
         super().__init__(master, **kwargs)
 
         self.item_list = items
@@ -37,7 +62,7 @@ class GroupItemsScrollableFrame(customtkinter.CTkScrollableFrame):
         # Add buttons to frame
         for index, list_item in enumerate(self.item_list):
             if list_item.get('icon'):
-                img = customtkinter.CTkImage(light_image=Image.open(self._open_icon(list_item.get('icon'))),
+                img = customtkinter.CTkImage(light_image=Image.open(_open_icon(list_item.get('icon'))),
                                              size=((int(24 * scale)), int((24 * scale)))) if list_item.get('icon') else None
                 icon = customtkinter.CTkLabel(self, text='', image=img)
                 icon.grid(row=index, column=0, padx=10, pady=10)
@@ -46,37 +71,26 @@ class GroupItemsScrollableFrame(customtkinter.CTkScrollableFrame):
             radio_button.grid(row=index, column=1, padx=10, pady=10, sticky='ew')
 
     def get_command(self):
-        """Returns the command of the selected radio button"""
+        """Returns the command associated with the selected radio button.
+
+        :return: The command string of the selected item
+        """
+
         return self.item_list[self.radio_var.get()].get('command')
-
-    def _open_icon(self, icon: str):
-        """Opens an icon and prepares it for PIL"""
-        if icon.endswith('.exe'):
-            try:
-                # Load icon
-                extractor = IconExtractor(icon)
-
-                # Read icon
-                data = extractor.get_icon(num=0)
-
-                return data
-
-            except IconExtractorError:
-                return None
-        else:
-            return icon
 
 class GroupInfoFrame(customtkinter.CTkFrame):
     """
-    Displays logo and text of a group
+    Displays the logo and text of a group
     """
     def __init__(self, master, icon: str, text: str, scale: float, title_font: customtkinter.CTkFont, **kwargs):
         """
-        :param master: Root frame
-        :param icon: Path to icon
-        :param text: Name of group
-        :param scale: Window scaling
+        :param master: Root frame or parent widget
+        :param icon: Path to the icon image file
+        :param text: The name of the group to be displayed
+        :param scale: Scaling factor for icon size
+        :param title_font: Font to be used for the group title text
         """
+
         super().__init__(master, **kwargs)
 
         # Configure grid
@@ -90,29 +104,13 @@ class GroupInfoFrame(customtkinter.CTkFrame):
         self.gi_title.grid(row=0, column=1, padx=10, pady=10, sticky='ew')
 
         # Configure icon
-        self.img = customtkinter.CTkImage(light_image=Image.open(self._open_icon(icon)),
+        self.img = customtkinter.CTkImage(light_image=Image.open(_open_icon(icon)),
                                           size=((int(32 * scale)), int((32 * scale)))) if icon else None
 
         # Spawn icon
         self.gi_icon = customtkinter.CTkLabel(self, text='', image=self.img)
         self.gi_icon.grid(row=0, column=0, padx=10, pady=10, sticky='w')
 
-    def _open_icon(self, icon: str):
-        """Opens an icon and prepares it for PIL"""
-        if icon.endswith('.exe'):
-            try:
-                # Load icon
-                extractor = IconExtractor(icon)
-
-                # Read icon
-                data = extractor.get_icon(num=0)
-
-                return data
-
-            except IconExtractorError:
-                return None
-        else:
-            return icon
 
 class ButtonFrame(customtkinter.CTkFrame):
     """
@@ -120,8 +118,11 @@ class ButtonFrame(customtkinter.CTkFrame):
     """
     def __init__(self, master, group_items_frame: GroupItemsScrollableFrame, base_font: customtkinter.CTkFont, **kwargs):
         """
-        :param master: Root frame
+        :param master: Root frame or parent widget
+        :param group_items_frame: Instance of `GroupItemsScrollableFrame` used to get the selected command
+        :param base_font: Font to be used for the button text
         """
+
         super().__init__(master, **kwargs)
 
         self.group_items_frame = group_items_frame
@@ -142,24 +143,26 @@ class ButtonFrame(customtkinter.CTkFrame):
         self.cancel_button.grid(row=0, column=1, padx=10, pady=10, sticky='ew')
 
     def cancel_button_command(self):
-        """Closes the app when the cancel button gets pressed"""
+        """Exits the application when the cancel button is pressed"""
+
         sys.exit(0)
 
     def continue_button_command(self):
-        """Opens the desired command selected by the radio buttons"""
+        """Executes the command associated with the selected radio button and then exits the application"""
         subprocess.Popen(self.group_items_frame.get_command(), shell=False)
         sys.exit(0)
 
 
 class App(customtkinter.CTk, group.DGFileGroup):
     """
-    Main window for opening a .desktopgroup file
+    Main window for opening a desktop group file
     """
     def __init__(self, dg_file: str, theme: str = None):
         """
-        :param dg_file: .desktopgroup file
-        :param theme: Path to theme file
+        :param dg_file: Path to the desktop group file containing the group data
+        :param theme: Optional path to a theme file to be used for styling the application
         """
+
         # Initialize CustomTkinter
         customtkinter.CTk.__init__(self)
 
@@ -214,7 +217,7 @@ class App(customtkinter.CTk, group.DGFileGroup):
         self.button_frame.grid(row=2, column=0, padx=10, pady=10, sticky='ew')
 
     def _set_geometry(self):
-        """Sets window geometry and centers window on screen"""
+        """Sets the window geometry, centers the window on the screen, and applies the scaling factor"""
 
         # Get screen width and height
         self.screen_width = self.winfo_screenwidth()
@@ -227,18 +230,27 @@ class App(customtkinter.CTk, group.DGFileGroup):
         self.geometry(self._window_geometry())
 
     def _calculate_window_position(self):
-        """Calculates window position"""
+        """Calculates the position of the window to center it on the screen, considering the scaling factor.
+
+        :return: A tuple (x, y) representing the position of the window
+        """
+
         win_x = int(((self.screen_width / 2) - (self.window_width / 2)) * self.scale_factor)
         win_y = (int(((self.screen_height / 2) - (self.window_height / 1.5)) * self.scale_factor) +
                  (80 * self.scale_factor))
         return win_x, win_y
 
     def _window_geometry(self):
-        """Creates geometry string"""
+        """Creates a string representing the window geometry (size and position)
+
+        :return: A geometry string in the format 'widthxheight+x+y'.
+        """
+
         return f'{self.window_width}x{self.window_height}+{self.window_x}+{self.window_y}'
 
     def _set_group_properties(self):
-        """Renders the group properties"""
+        """Sets the window title and icon based on the group properties. Uses a default icon if none is provided."""
+
         self.title(self.name)
 
         if self.icon:
@@ -260,7 +272,7 @@ class App(customtkinter.CTk, group.DGFileGroup):
                     self.protocol("WM_DELETE_WINDOW", on_close)
 
     def _create_font_objects(self):
-        """Creates objects for different font types"""
+        """Creates font objects for base text and titles using the system default font"""
 
         # Get system font
         default_font = font.nametofont('TkDefaultFont')
